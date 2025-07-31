6dee874987bd71f8a2e17619f1d8c6365789dde5
elrollins <elmerrollins352@gmail.com>
2025-07-31 19:02:35 -0400

    finished project

--- froyo.js
+++ froyo.js
@@ -1,3 +1,17 @@
-let dog = "5";
+// made prompt //
+const input = prompt("Enter a list of froyo flavors, separated by commas:");
+// turned input into an array //
+const flavors = input.split(",");
 
-console.log(dog);
+const flavorCounter = {};
+// allowing flavors to have a count based on the input //
+for (let i = 0; i < flavors.length; i++) {
+  const flavor = flavors[i];
+  if (flavor in flavorCounter) {
+    flavorCounter[flavor]++;
+  } else {
+    flavorCounter[flavor] = 1;
+  }
+}
+
+console.log(flavorCounter);
